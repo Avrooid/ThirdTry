a234d566439bc0cf9fab13277be7142aaa45863d
Nikita <nikitaburakov08@gmail.com>
2022-09-07 19:41:58 +0500

    fix ThirdTry

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("НУ ВЫ И ДУРАЧКИ!");
+        int i = 1;
+        System.out.println(i);
+    }
+}
